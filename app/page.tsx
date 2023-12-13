48e3edcce154e3db2e999214fcf81357a773f761
Garrett Tolbert <garrett@vercel.com>
2023-12-13 10:27:37 -0600

    force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
 	const data = await fetch('https://api.github.com/users/leerob').then((res) => res.json());
 
